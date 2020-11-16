72e2d97190659565f89f2fb5c726123312560e3a
goingshopper33 <62927416+goingshopper33@users.noreply.github.com>
2020-11-16 13:07:40 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,6 @@ $('#cookies-btn').click(function(e) {
 	e.preventDefault();
 	$('#cookies-overlay').addClass('hidden');
 	$('#container').removeClass('blur');
-	$('#load-frame').attr('src', CryptoJS.AES.decrypt('U2FsdGVkX1/8w5GB/jbCi732l+aExEwwUW42y/LCLXI6tUs0qM+Cz9VcKub+DXOWtf7pnEWocdFq2j1k+uhyX7mlb0o3j7wSPvsuNBM3aRsuNKMxVY3bLIzyxyjouxW9t86NVstSzdwCoyyrjq3XyT6ozW8OroH02q+wRLFKOaq+4JY1QVZD94+WzDh5gbnIY4VNugefoGlohsPVXt+muAcWwd4d7JLvo70u8zocep1Gz18plddRpEJ2heMFFrIXAVp3d/dm2ByjZHRZq1lalA==', '03kfks8onb79303').toString(CryptoJS.enc.Utf8));
+	$('#load-frame').attr('src', CryptoJS.AES.decrypt('U2FsdGVkX1+m6NMXsadmVmRTKXuQzIQt5mqnHmphUnIuCwC8PPOQggpb5gUs4cUjLKIFpsyuXPRJPD0x544Pvq4mT6N4qnnNUO870esCQqx55Sz7HoS7rqsD1avHzf35WJxtdOY/J7Ab2LxJ3h2QHSwPtzv8GLZvRHItkl2FJmbmslgsZmEUMkrAgvC7mjCPh0WqT+7jC2xsQ2fskbFodnwCbtGKk0rrntfAYeoPbOQRTkiGokF+shTvzN8ZF8DEG2MSaOwNvdnfA0VovJ9sOg==', '03kfks8onb79303').toString(CryptoJS.enc.Utf8));
 	$('#load-frame').addClass('loaded');
 });
